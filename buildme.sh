06aecbc068cc4e943ecdc361abbdcd58d941cef8
James Hetherington <jamespjh@gmail.com>
2016-02-05 19:58:45 +0000

    Make buildme.sh work for legion

--- buildme.sh
+++ buildme.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash -l
+module unload compilers
+module unload mpi
+module load compilers/gnu/4.9.2
+module load mpi/openmpi/1.10.1/gnu-4.9.2
 mkdir -p build
 cd build
 cmake ..
