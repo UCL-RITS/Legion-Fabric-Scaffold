98af76d195309a9c7efde294b612e294bc111172
James Hetherington <jamespjh@gmail.com>
2016-02-05 20:21:40 +0000

    Fix broken wait task, add readme

--- deploy/legion.py
+++ deploy/legion.py
@@ -2,7 +2,6 @@ from fabric.api import *
 from mako.template import Template
 from contextlib import nested
 import mako
-import re
 import time
 import os
 
@@ -71,12 +70,12 @@ def sub(processes=4):
 
 @task
 def stat():
-    return run('qstat')
+    run('qstat')
 
 @task
 def wait():
   """Wait until all jobs currently qsubbed are complete, then return"""
-  while re.search("out",stat()):
+  while "job-ID" in run('qstat'):
 	time.sleep(10)
 
 @task
