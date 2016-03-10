0f28a6c44efd2d32b0cb2c254583415c1c1bbad4
James Hetherington <jamespjh@gmail.com>
2016-03-10 18:10:48 +0000

    Catch example for OpenCL

--- test/catch.cpp
+++ test/catch.cpp
@@ -3,6 +3,7 @@
 
 #include "catch.hpp"
 #include <cmath>
+#include "build.h"
 
 int main(int argc, char * argv[]) {
     int result = Catch::Session().run(argc, argv);
@@ -12,9 +13,27 @@ int main(int argc, char * argv[]) {
 
 
 
-TEST_CASE( "Model can be instantiated and configured", "[Model]" ) {
+TEST_CASE( "Basic OpenCL compilation and dispatch", "[Builder]" ) {
 
-    SECTION( "Model can be constructed" ) {
 
+    Builder b;
+    b.SelectDevice(0);
+    b.Program("test/catch_cl.cl");
+    cl_kernel trivial = b.CreateKernel("trivial");
+
+    SECTION( "Kernel can be run"){
+
+      cl_mem outputBuffer = b.AllocateDeviceMemory(CL_MEM_WRITE_ONLY, 1);
+      int results[1];
+
+      // Bind buffer to kernel argument
+      cl_int result = clSetKernelArg(trivial, 0, sizeof(cl_mem), &outputBuffer);
+
+      b.DispatchKernel(trivial, 1);
+
+      b.DeviceToHost(outputBuffer, results, 1);
+      REQUIRE(result == CL_SUCCESS);
+      REQUIRE(results[0]==7);
     }
+
 }
--- test/catch_cl.cl
+++ test/catch_cl.cl
@@ -1,22 +1,3 @@
-// Next line tells CATCH we will use our own main function
-#define CATCH_CONFIG_RUNNER
-#include <cmath>
-#include <thrust/device_vector.h>
-#include "catch.hpp"
-#include "Model.h"
-#include "helpers.h"
-
-
-int main(int argc, char * argv[]) {
-    Builder b();
-}
-
-
-
-TEST_CASE ("OpenCL Tests"){
-
-    SECTION("Basic openCL dispatch works"){
-
-
-    }
+__kernel void trivial(__global int * result){
+  *result = 7;
 }
